2d2509652e6868c0773876054c866b2dcc17f6b1
Allan Pearson <Alprsn2012@gmail.com>
2019-06-29 00:01:41 -0500

    tags added to lines 10 -25 function

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,5 +1,5 @@
 
-    var alphabet = ["i", "b", "c", "o", "n", "t", "i"]
+    var letters = ["i", "b", "c", "o", "n", "t", "i"]
 , randomLetter = ""
 , winCount = 0
 , lossCount = 0
@@ -7,6 +7,21 @@
 , guessedLetters = []
 , defaultGuessesLeft =7;
 window.addEventListener("keypress", onKeyPress, false); newGame();
+
+function newGame(){
+    guessedLetters = [];
+    resetGuesses();
+    generateRandowLetter();
+    displayOnScreen("wins", "wins: ${String(wincount)}");
+    displayOnScreen("losses", "losses: ${String(lossCount)}");
+    displayOnScreen("Guessesleft",  Guesses "Left: ${String (guessesLeft)}");
+}
+function onKeyPress(key) {
+    letter - key.key.tolowerCase();
+    if (letters.includes(letter, 0) && quessedLetters.includes(letter, 0) ===false)[
+        guessedLetters.push(letter);
+        printKeyPressed(letter);
+}
 
                     
 
